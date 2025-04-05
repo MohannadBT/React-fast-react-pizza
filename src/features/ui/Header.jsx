7268759a92f173ad2337ea951bc4858399abaafb
MohannadBT <xsmile3x@gmail.com>
2025-04-05 03:39:47 +0300

    Finish section 23

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../order/SearchOrder";
+import UserName from "../user/UserName";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast react Pizza Co.</Link>
+    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast react Pizza Co.
+      </Link>
 
       <SearchOrder />
+
+      <UserName />
     </header>
   );
 }
